16b6a7a4e37f729ff5b678d9eca5fe2ecdec7c07
chandusiriyala <chandusiriyala116@gmail.com>
2024-04-26 01:11:55 +0530

    Update EditorPage.js

--- EditorPage.js
+++ EditorPage.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 const EditorPage = () => {
     return (
-        <h1>Editor  Page</h1>
-    )
-}
+         <div className='mainWrap'>
+            <div className = "aside">
+                <div className = "asideInner">
+                    <div className = "logo">
+                    <img className= "logo"src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0IkXDh3-w50N8hru0jrbEJ6dOu3LqiMoTE7ahVKT17g&s" alt = "CodeRkv"/>
+                    </div>
+                    <h3>Connected</h3>
+                    <div className='clientList'>
+
+                    </div>
+                </div>
+                
+            </div>
+            <div className='editorWrap'>
+                editor goes on ......
+            </div>
+         </div>
+    );
+};
 
 export default EditorPage
